0ba88fda113305e7d26cb015ce71f3addd7076af
Mawel Mirabal <mmirabalp@gmail.com>
2018-01-10 05:18:51 -0800

    Added Create and Delete Note Buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('test');
+});
+
+
 const title = document.getElementById('title');
 
 const description =document.getElementById('description');
@@ -14,4 +19,75 @@ for (var i = 0; i < items.length; i++) {
 
 
 const form = document.getElementById('form');
-console.log(form);
+const row = document.querySelector('.row');
+
+form.addEventListener('submit', function(event){
+	event.preventDefault();
+	let title_form = document.getElementById('title-form').value;
+	let content_form = document.getElementById('content-form').value;
+	// console.log(title_form);
+	// console.log(content_form);
+
+	create_card(title_form, content_form);
+});
+form.addEventListener('change', function(event){
+	console.log('change value');
+});
+
+let div = null;
+function create_card(title, content){
+
+	    div = document.createElement('div');
+		div.className = 'col-sm-6 col-md-4';
+
+	let thumbnail = document.createElement('div');
+		thumbnail.className = 'thumbnail';
+
+	let caption = document.createElement('div');
+		caption.className = 'caption';
+
+	let h3 = document.createElement('h3');
+		h3.textContent = title;
+
+	let p1 = document.createElement('p');
+		p1.textContent = content;
+
+	let p2 = document.createElement('p');
+	let a = document.createElement('a');
+		a.className = 'btn btn-danger';
+		a.textContent = 'Delete';
+		p2.addEventListener('click', delete_card);
+
+	p2.appendChild(a);
+	caption.appendChild(h3);
+	caption.appendChild(p1);
+	caption.appendChild(p2);
+	thumbnail.appendChild(caption);
+	div.appendChild(thumbnail);
+	row.appendChild(div);
+}
+
+function delete_card(event){
+	let parent = get_parent(event.target, 4);
+	row.removeChild(parent);
+
+}
+
+function get_parent(parent, level){
+	if(level == 0 ){
+		return parent;
+	}
+	level--;
+	return get_parent(parent.parentElement, level);
+}
+
+
+
+
+
+
+
+
+
+
+
